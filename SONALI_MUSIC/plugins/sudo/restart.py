436af0de59dec4c756db49fe05617ef229ffc22c
𝐀𝐤𝐚𝐬𝐡 𝐃𝐚𝐤𝐬𝐡𝐰𝐚𝐧𝐬𝐡𝐢 : ZoxxOP <akp954834@gmail.com>
2025-11-20 07:54:23 +0530

    Update restart.py

--- SONALI_MUSIC/plugins/sudo/restart.py
+++ SONALI_MUSIC/plugins/sudo/restart.py
@@ -27,7 +27,11 @@ async def is_heroku():
     return "heroku" in socket.getfqdn()
 
 
-@app.on_message(filters.command(["getlog", "logs", "getlogs"], prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & filters.user(8143754205))
+# ---------------- LOGS ---------------- #
+@app.on_message(
+    filters.command(["getlog", "logs", "getlogs"], prefixes=["/", "!", "%", ",", ".", "@", "#"])
+    & filters.user(SUDOERS)
+)
 @language
 async def log_(client, message, _):
     try:
@@ -36,102 +40,110 @@ async def log_(client, message, _):
         await message.reply_text(_["server_1"])
 
 
-@app.on_message(filters.command(["update", "gitpull"], prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & filters.user(8143754205))
+# ---------------- UPDATE / GITPULL ---------------- #
+@app.on_message(
+    filters.command(["update", "gitpull"], prefixes=["/", "!", "%", ",", ".", "@", "#"])
+    & filters.user(SUDOERS)
+)
 @language
 async def update_(client, message, _):
-    if await is_heroku():
-        if HAPP is None:
-            return await message.reply_text(_["server_2"])
+
+    if await is_heroku() and HAPP is None:
+        return await message.reply_text(_["server_2"])
+
     response = await message.reply_text(_["server_3"])
+
     try:
         repo = Repo()
     except GitCommandError:
         return await response.edit(_["server_4"])
     except InvalidGitRepositoryError:
         return await response.edit(_["server_5"])
-    to_exc = f"git fetch origin {config.UPSTREAM_BRANCH} &> /dev/null"
-    os.system(to_exc)
-    await asyncio.sleep(7)
-    verification = ""
-    REPO_ = repo.remotes.origin.url.split(".git")[0]
-    for checks in repo.iter_commits(f"HEAD..origin/{config.UPSTREAM_BRANCH}"):
-        verification = str(checks.count())
-    if verification == "":
-        return await response.edit(_["server_6"])
+
+    os.system(f"git fetch origin {config.UPSTREAM_BRANCH} &> /dev/null")
+    await asyncio.sleep(5)
+
     updates = ""
-    ordinal = lambda format: "%d%s" % (
-        format,
-        "tsnrhtdd"[(format // 10 % 10 != 1) * (format % 10 < 4) * format % 10 :: 4],
-    )
-    for info in repo.iter_commits(f"HEAD..origin/{config.UPSTREAM_BRANCH}"):
-        updates += f"<b>➣ #{info.count()}: <a href={REPO_}/commit/{info}>{info.summary}</a> ʙʏ -> {info.author}</b>\n\t\t\t\t<b>➥ ᴄᴏᴍᴍɪᴛᴇᴅ ᴏɴ :</b> {ordinal(int(datetime.fromtimestamp(info.committed_date).strftime('%d')))} {datetime.fromtimestamp(info.committed_date).strftime('%b')}, {datetime.fromtimestamp(info.committed_date).strftime('%Y')}\n\n"
-    _update_response_ = "<b>ᴀ ɴᴇᴡ ᴜᴩᴅᴀᴛᴇ ɪs ᴀᴠᴀɪʟᴀʙʟᴇ ғᴏʀ ᴛʜᴇ ʙᴏᴛ !</b>\n\n➣ ᴩᴜsʜɪɴɢ ᴜᴩᴅᴀᴛᴇs ɴᴏᴡ\n\n<b><u>ᴜᴩᴅᴀᴛᴇs:</u></b>\n\n"
-    _final_updates_ = _update_response_ + updates
-    if len(_final_updates_) > 4096:
-        url = await SonaBin(updates)
-        nrs = await response.edit(
-            f"<b>ᴀ ɴᴇᴡ ᴜᴩᴅᴀᴛᴇ ɪs ᴀᴠᴀɪʟᴀʙʟᴇ ғᴏʀ ᴛʜᴇ ʙᴏᴛ !</b>\n\n➣ ᴩᴜsʜɪɴɢ ᴜᴩᴅᴀᴛᴇs ɴᴏᴡ\n\n<u><b>ᴜᴩᴅᴀᴛᴇs :</b></u>\n\n<a href={url}>ᴄʜᴇᴄᴋ ᴜᴩᴅᴀᴛᴇs</a>"
-        )
+    repo_url = repo.remotes.origin.url.split(".git")[0]
+
+    for commit in repo.iter_commits(f"HEAD..origin/{config.UPSTREAM_BRANCH}"):
+        day = int(datetime.fromtimestamp(commit.committed_date).strftime('%d'))
+        date = datetime.fromtimestamp(commit.committed_date).strftime('%b %Y')
+        updates += f"• <a href='{repo_url}/commit/{commit.hexsha}'>{commit.summary}</a> — {day} {date}\n"
+
+    if not updates:
+        return await response.edit(_["server_6"])
+
+    if len(updates) > 4000:
+        link = await SonaBin(updates)
+        await response.edit(f"Updates too long, check here:\n{link}")
     else:
-        nrs = await response.edit(_final_updates_, disable_web_page_preview=True)
+        await response.edit(updates, disable_web_page_preview=True)
+
     os.system("git stash &> /dev/null && git pull")
 
+    # notify chats
     try:
-        served_chats = await get_active_chats()
-        for x in served_chats:
+        active = await get_active_chats()
+        for chat in active:
             try:
-                await app.send_message(
-                    chat_id=int(x),
-                    text=_["server_8"].format(app.mention),
-                )
-                await remove_active_chat(x)
-                await remove_active_video_chat(x)
+                await app.send_message(chat, _["server_8"].format(app.mention))
+                await remove_active_chat(chat)
+                await remove_active_video_chat(chat)
             except:
                 pass
-        await response.edit(f"{nrs.text}\n\n{_['server_7']}")
+        await response.edit(f"{updates}\n\n{_['server_7']}")
     except:
         pass
 
+    # Restart bot
     if await is_heroku():
         try:
             os.system(
-                f"{XCB[5]} {XCB[7]} {XCB[9]}{XCB[4]}{XCB[0]*2}{XCB[6]}{XCB[4]}{XCB[8]}{XCB[1]}{XCB[5]}{XCB[2]}{XCB[6]}{XCB[2]}{XCB[3]}{XCB[0]}{XCB[10]}{XCB[2]}{XCB[5]} {XCB[11]}{XCB[4]}{XCB[12]}"
+                f"{XCB[5]} {XCB[7]} {XCB[9]}{XCB[4]}{XCB[0]*2}"
+                f"{XCB[6]}{XCB[4]}{XCB[8]}{XCB[1]}{XCB[5]}"
+                f"{XCB[2]}{XCB[6]}{XCB[2]}{XCB[3]}{XCB[0]}"
+                f"{XCB[10]}{XCB[2]}{XCB[5]} {XCB[11]}{XCB[4]}{XCB[12]}"
             )
             return
         except Exception as err:
-            await response.edit(f"{nrs.text}\n\n{_['server_9']}")
+            await response.edit(f"{updates}\n\n{_['server_9']}")
             return await app.send_message(
-                chat_id=config.LOGGER_ID,
-                text=_["server_10"].format(err),
+                config.LOGGER_ID,
+                _["server_10"].format(err),
             )
     else:
         os.system("pip3 install -r requirements.txt")
-        os.system(f"kill -9 {os.getpid()} && bash start")
-        exit()
+        os.kill(os.getpid(), 9)
 
 
-@app.on_message(filters.command(["restart"]) & filters.user(8143754205))
+# ---------------- RESTART ---------------- #
+@app.on_message(
+    filters.command(["restart"], prefixes=["/", "!", "%", ",", ".", "@", "#"])
+    & filters.user(SUDOERS)
+)
 async def restart_(_, message):
-    response = await message.reply_text("ʀᴇsᴛᴀʀᴛɪɴɢ...")
-    ac_chats = await get_active_chats()
-    for x in ac_chats:
+
+    response = await message.reply_text("Restarting...")
+
+    active = await get_active_chats()
+    for chat in active:
         try:
             await app.send_message(
-                chat_id=int(x),
-                text=f"{app.mention} ɪs ʀᴇsᴛᴀʀᴛɪɴɢ...\n\nʏᴏᴜ ᴄᴀɴ sᴛᴀʀᴛ ᴩʟᴀʏɪɴɢ ᴀɢᴀɪɴ ᴀғᴛᴇʀ 15-20 sᴇᴄᴏɴᴅs.",
+                chat,
+                f"{app.mention} is restarting… Please wait 15-20 seconds."
             )
-            await remove_active_chat(x)
-            await remove_active_video_chat(x)
+            await remove_active_chat(chat)
+            await remove_active_video_chat(chat)
         except:
             pass
 
-    try:
-        shutil.rmtree("downloads")
-        shutil.rmtree("raw_files")
-        shutil.rmtree("cache")
-    except:
-        pass
-    await response.edit_text(
-        "» ʀᴇsᴛᴀʀᴛ ᴘʀᴏᴄᴇss sᴛᴀʀᴛᴇᴅ, ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ ғᴏʀ ғᴇᴡ sᴇᴄᴏɴᴅs ᴜɴᴛɪʟ ᴛʜᴇ ʙᴏᴛ sᴛᴀʀᴛs..."
-    )
-    os.system(f"kill -9 {os.getpid()} && bash start")
+    for folder in ["downloads", "raw_files", "cache"]:
+        try:
+            shutil.rmtree(folder)
+        except:
+            pass
+
+    await response.edit("Restart process started…")
+
+    os.kill(os.getpid(), 9)
