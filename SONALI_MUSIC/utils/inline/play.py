56db68130f265beab163bdddc35ed4f05fec71b4
ignore528 <aliakram7548@gmail.com>
2026-02-16 14:42:51 +0300

    Fix typo in button text from 'ᴀʙᴜᴛ' to 'ᴀʙᴏᴜᴛ'

--- SONALI_MUSIC/utils/inline/play.py
+++ SONALI_MUSIC/utils/inline/play.py
@@ -65,7 +65,7 @@ def stream_markup_timer(_, chat_id, played, dur):
         ],
         [
          InlineKeyboardButton(text="• ᴏᴡɴᴇʀ •", url="t.me/IGNORE_SHEIKH"),
-         InlineKeyboardButton(text="• ᴀʙᴜᴛ •", url="https://t.me/FalakAbout"),
+         InlineKeyboardButton(text="• ᴀʙᴏᴜᴛ •", url="https://t.me/FalakAbout"),
          InlineKeyboardButton(text="• ɢʀᴏᴜᴘ •", url="https://t.me/+ijVUQc7TNEUwNmNl"),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
